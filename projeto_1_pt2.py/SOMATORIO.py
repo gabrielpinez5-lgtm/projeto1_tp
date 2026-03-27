07fa8e9a77a18ccffa0f3a44dc17ae1d981d734c
Gabriel Pinez <gabrielpinez5@gmail.com>
2026-03-27 08:17:44 -0300

    Update SOMATORIO.py

--- projeto_1_pt2.py/SOMATORIO.py
+++ projeto_1_pt2.py/SOMATORIO.py
@@ -4,25 +4,41 @@ class Somatorio_class():
         self.soma_pesos = 0.0
         self.qnt_valores_somados = 0
         self.soma_valor_com_peso = 0.0
+        self.soma_inversos = 0.0
 
-    def numero_valores(self, num):
-        self.qnt_valores_somados = num
 
-    def aritimetica(self):
-        return self.soma_valores / self.qnt_valores_somados
     
-    def ponderada(self):
-        self.soma_valor_com_peso
 
-    def somar(self, valor):
-        pass
+
+
+    def somar_valores(self, valor):
+        self.soma_valores += valor
+        self.qnt_valores_somados += 1
+        
+        
 
     def somar_valor_com_peso(self, valor, peso):
         self.soma_valor_com_peso += valor * peso
         self.soma_pesos += peso
-        self.qnt_valores_somados += 1
+
+
+    def somar_inversos(self, valor):
+        if valor != 0:
+            self.soma_inversos += 1 / valor
+        else:
+            raise ZeroDivisionError("Divisao por zero!")
 
     @property
     def media_ponderada(self):
         return self.soma_valor_com_peso / self.soma_pesos
+    
+    @property
+    def media_harmonica(self):
+        if self.soma_inversos == 0:
+            raise ZeroDivisionError("A soma dos inversos é zero!")
+        return self.qnt_valores_somados / self.soma_inversos
+
+    @property
+    def media_geometrtica(self):
+        return 
         
